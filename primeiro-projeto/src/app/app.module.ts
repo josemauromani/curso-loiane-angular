609b50e66ef914a011b9c6578f3b03aec68a6591
dermoagenda <josemauromani@gmail.com>
2020-06-30 14:52:33 -0300

    modulo cursos

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppComponent } from './app.component';
 
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { MeuSegundoComponent } from './meu-segundo/meu-segundo.component';
+import { CursosModule } from './cursos/cursos.module';
+
+// DECORATOR QUE INFORMA QUE ESSA CLASSE É UM MODULO
 @NgModule({
   declarations: [
     AppComponent,
     MeuPrimeiroComponent,
-    MeuSegundoComponent
+    MeuSegundoComponent,
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, // prepara a aplicação para ser executada em um browser
+    AppRoutingModule,
+    CursosModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] // qual o componente que vai servir como container do projeto todo
 })
 export class AppModule { }
---
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { CursosService } from './cursos.service';
+
+@Component({
+  selector: 'app-cursos',
+  templateUrl: './cursos.component.html',
+  styleUrls: ['./cursos.component.css']
+})
+export class CursosComponent implements OnInit {
+
+  nomePortal: String;
+  cursos: String[];
+
+  constructor(private cursosService: CursosService) {
+    this.nomePortal = 'http://loiane.training';
+    this.cursos = cursosService.getCursos();
+  }
+  
+  ngOnInit(): void {
+  }
+
+}
