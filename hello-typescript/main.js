f32105ef87f95e6b118066939b58b609372deac3
dermoagenda <josemauromani@gmail.com>
2020-06-30 14:52:52 -0300

    hello typescript

---
+++ hello-typescript/main.js
@@ -0,0 +1,14 @@
+var minhaVar = 'my var';
+function minhaFuncao(x, y) {
+    return x + y;
+}
+var num = 2;
+var PI = 3.14;
+var Matematica = /** @class */ (function () {
+    function Matematica() {
+    }
+    Matematica.prototype.soma = function (x, y) {
+        return x + y;
+    };
+    return Matematica;
+}());
---
+++ hello-typescript/main.ts
@@ -0,0 +1,17 @@
+var minhaVar = 'my var';
+
+function minhaFuncao(x,y) {
+  return x + y;
+}
+
+let num = 2 ;
+const PI = 3.14;
+
+class Matematica {
+  soma(x,y) {
+    return x+y;
+  }
+}
+
+var n1:string = 'teste'
+//n1 = 4;
